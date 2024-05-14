14385c57827d63b631b2e8b484aa9f120b67c6ca
vincent d warmerdam <vincentwarmerdam@gmail.com>
2024-05-14 12:34:28 +0000

    wrap

--- playtime/formula.py
+++ playtime/formula.py
@@ -1,46 +1,15 @@
 import numpy as np 
+import polars as pl
 from time import time as timer
 import itertools as it 
 from sklearn.base import clone, BaseEstimator, MetaEstimatorMixin
 from sklearn.pipeline import make_pipeline, make_union, FeatureUnion
-from sklearn.preprocessing import PolynomialFeatures, SplineTransformer, OneHotEncoder
+from sklearn.preprocessing import PolynomialFeatures, SplineTransformer, OneHotEncoder, FunctionTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 from skrub import SelectCols
 from scipy.sparse import hstack, coo_array, issparse, csc_array
 
 
-# class UnionPolynomialFeaturizer(BaseEstimator, MetaEstimatorMixin):
-#     def __init__(self, union_estimator):
-#         """Like the normal polynomial featurizer, but in this case only consider the product from different transformers"""
-#         self.union_estimator = union_estimator
-        
-#     def _combinations(self, n_features, min_degree, max_degree, interaction_only, include_bias):
-#         for i in range(self.split_at_):
-#             for j in range(self.split_at_, self.shape_out_):
-#                 yield (i, j)
-    
-#     def _num_combinations(self, n_features, min_degree, max_degree, interaction_only, include_bias):
-#         total = 0
-#         for i in range(self.split_at_):
-#             for j in range(self.split_at_, self.shape_out_):
-#                 total += 1 
-#         return total
-
-#     def fit(self, X, y=None):
-#         self.vendored_poly_ = PolynomialFeatures(interaction_only=True)
-#         self.vendored_poly_._combinations = self._combinations
-#         X_tfm = self.union_estimator.fit_transform(X)
-#         total = X_tfm.shape[1]
-#         self.vendored_poly_.fit(X_tfm)
-#         # todo: kind of hacky. clean? 
-#         self.split_at_ = self.union_estimator.transformer_list[0][1].transform(X).shape[1]
-#         self.shape_out_ = self.split_at_ * (total - self.split_at_)
-#         return self
-
-#     def transform(self, X):
-#         X_tfm = self.union_estimator.transform(X)
-#         return self.vendored_poly_.transform(X_tfm)
-    
-    
 class UnionPolynomialFeaturizer(BaseEstimator, MetaEstimatorMixin):
     def __init__(self, union_estimator):
         """Like the normal polynomial featurizer, but in this case only consider the product from different transformers"""
@@ -80,24 +49,45 @@ class Node:
             new_pipeline = make_union(self.pipeline, other.pipeline)
         return Node(pipeline=new_pipeline)
 
-    def __mul__(self, other):
-        combined = UnionPolynomialFeaturizer(make_union(clone(self.pipeline), clone(other.pipeline))) 
-        return Node(pipeline=combined)
+    def fit(self, X, y=None, **kwargs):
+        self.pipeline.fit(X, y, **kwargs)
+        return self
 
+    def transform(self, X, y=None, **kwargs):
+        return self.pipeline.transform(X, y, **kwargs)
 
-def seasonal(colname):
+    def fit_transform(self, X, y=None, **kwargs):
+        return self.pipeline.fit_transform(X, y, **kwargs)
+
+def datetime_feats(dataf, column):
+    nw_df = nw.from_native(dataf)
+    
+    nw_out = nw_df.with_columns(
+        day_of_year=nw.col(column).str.to_datetime(format="%Y-%m-%d").dt.ordinal_day()
+    ).select("day_of_year")
+    
+    return nw.to_native(nw_out)
+
+def column_pluck(dataf, column):
+    return pl.DataFrame(dataf)[column].to_list()
+
+def seasonal(colname, n_knots=12):
     return Node(pipeline=make_pipeline(
-        SelectCols([colname]),
-        SplineTransformer(extrapolation="periodic", knots="uniform", n_knots=5)
+        FunctionTransformer(datetime_feats, kw_args={'column': colname}),
+        SplineTransformer(extrapolation="periodic", knots="uniform", n_knots=n_knots)
     ))
 
 def feats(*colnames):
     return Node(pipeline=SelectCols([col for col in colnames]))
 
 def dummy(*colnames):
-    return Node(pipeline=make_union(
-        *[make_pipeline(SelectCols(col), OneHotEncoder()) for col in colnames]
-    ))
+    return Node(pipeline=make_pipeline(SelectCols(colnames), OneHotEncoder()))
 
 def time(colname):
     return Node(pipeline=SelectCols([colname]))
+
+def bag_of_words(colname, **kwargs):
+    return Node(pipeline=make_pipeline(
+        FunctionTransformer(column_pluck, kw_args={'column': colname}),
+        CountVectorizer(**kwargs)
+    ))
