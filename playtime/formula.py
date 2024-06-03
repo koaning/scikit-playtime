b710df0a98b0864a0f0750baddaa2b1f8b18dc34
vincent d warmerdam <vincentwarmerdam@gmail.com>
2024-06-03 17:53:33 +0200

    added

--- playtime/formula.py
+++ playtime/formula.py
@@ -1,10 +1,11 @@
+import itertools as it
 import narwhals as nw
 import numpy as np
 import polars as pl
 from scipy.sparse import csc_array, hstack, issparse
-from sklearn.base import BaseEstimator, MetaEstimatorMixin, clone
+from sklearn.base import MetaEstimatorMixin, clone, BaseEstimator, TransformerMixin
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.pipeline import FeatureUnion, make_pipeline, make_union
+from sklearn.pipeline import FeatureUnion, make_pipeline, make_union, Pipeline
 from sklearn.preprocessing import FunctionTransformer, OneHotEncoder, SplineTransformer
 from skrub import SelectCols
 
@@ -37,6 +38,40 @@ class UnionPolynomialFeaturizer(BaseEstimator, MetaEstimatorMixin):
         return np.hstack(columns, dtype=X_tfm.dtype)
 
 
+class CrossPolyPipeline(BaseEstimator, TransformerMixin):
+    def __init__(self, union_estimator):
+        self.union_estimator = union_estimator
+        self.transformer_list = union_estimator.transformer_list
+
+    def fit(self, X, y=None):
+        self.split_marks_ = {}
+        start = 0
+        self.union_estimator.fit(X)
+        for name, tfm in self.union_estimator.transformer_list:
+            width = tfm.transform(X[:2]).shape[1]
+            self.split_marks_[name] = slice(start, start + width)
+            start = width + start
+
+        self.shape_out_ = start
+        return self
+
+    def transform(self, X):
+        columns = []
+        X_tfm = self.union_estimator.transform(X)
+        if issparse(X_tfm):
+            X_tfm = csc_array(X_tfm)
+        for name1, name2 in it.combinations(self.split_marks_.keys(), 2):
+            X1, X2 = (
+                X_tfm[:, self.split_marks_[name1]],
+                X_tfm[:, self.split_marks_[name2]],
+            )
+        for x in range(X2.shape[1]):
+            columns.append(X1 * X2[:, [x]])
+        if issparse(X_tfm):
+            return hstack(columns, dtype=X_tfm.dtype).tocsc()
+        return np.hstack(columns, dtype=X_tfm.dtype)
+
+
 class PlaytimePipeline(BaseEstimator):
     def __init__(self, pipeline):
         self.pipeline = pipeline
@@ -49,6 +84,22 @@ class PlaytimePipeline(BaseEstimator):
         else:
             new_pipeline = make_union(self.pipeline, other.pipeline)
         return PlaytimePipeline(pipeline=new_pipeline)
+
+    def __mul__(self, other):
+        if not isinstance(self, CrossPolyPipeline) and not isinstance(
+            other, CrossPolyPipeline
+        ):
+            # This is a special case, it is rare, but they could both be cross prod already
+            unioned_feats = (self + other).pipeline
+        elif isinstance(other.pipeline, FeatureUnion):
+            # We know that the self is the CrossPolyPipeline in this case
+            old_transformers = [_[1] for _ in self.pipeline.transformer_list]
+            unioned_feats = old_transformers + [clone(other.pipeline)]
+        elif isinstance(self.pipeline, (Pipeline, FeatureUnion)):
+            # We know that the other is the CrossPolyPipeline in this case
+            old_transformers = [_[1] for _ in other.pipeline.transformer_list]
+            unioned_feats = old_transformers + [clone(self.pipeline)]
+        return PlaytimePipeline(pipeline=CrossPolyPipeline(unioned_feats))
 
     def fit(self, X, y=None, **kwargs):
         self.pipeline.fit(X, y, **kwargs)
@@ -87,7 +138,9 @@ def seasonal(colname, n_knots=12):
 
 
 def feats(*colnames):
-    return PlaytimePipeline(pipeline=SelectCols([col for col in colnames]))
+    return PlaytimePipeline(
+        pipeline=make_pipeline(SelectCols([col for col in colnames]))
+    )
 
 
 def onehot(*colnames):
