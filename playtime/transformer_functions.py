4c4abe539942b4e71a2492ee0143f5d0245cbfbf
vincent d warmerdam <vincentwarmerdam@gmail.com>
2024-11-20 11:28:05 +0000

    more-seasons

--- playtime/transformer_functions.py
+++ playtime/transformer_functions.py
@@ -2,11 +2,20 @@ import polars as pl
 import narwhals as nw
 
 
-def datetime_feats(dataf, column):
+def datetime_feats(dataf, column, kind="days"):
     nw_df = nw.from_native(dataf)
-    nw_out = nw_df.with_columns(
-        day_of_year=nw.col(column).str.to_datetime(format="%Y-%m-%d").dt.ordinal_day()
-    ).select("day_of_year")
+    if kind == "days":
+        nw_out = nw_df.with_columns(
+            day_of_year=nw.col(column).dt.ordinal_day()
+        ).select("_dt_feat")
+    if kind == "hours":
+        nw_out = nw_df.with_columns(
+            hour_of_day=nw.col(column).dt.hour()
+        ).select("_dt_feat")
+    if kind == "minutes":
+        nw_out = nw_df.with_columns(
+            minute_of_hour=nw.col(column).dt.hour()
+        ).select("_dt_feat")
 
     return nw.to_native(nw_out)
 
